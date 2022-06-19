6b702598f356bbd8b3be92f86a2480b16390c7b5
Carlos Machel <carlostiago@outlook.com.br>
2022-06-19 14:31:19 -0300

    MassTransit Starting With Mediator

---
+++ Sample.Api/Program.cs
@@ -0,0 +1,48 @@
+using MassTransit;
+using Sample.Components;
+using Sample.Contracts;
+using Serilog;
+using Serilog.Events;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((host, log) =>
+{
+    if (host.HostingEnvironment.IsProduction())
+        log.MinimumLevel.Information();
+    else
+        log.MinimumLevel.Debug();
+
+    log.WriteTo.Console();
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddMediator(cfg =>
+{
+    cfg.AddConsumer<SubmitOrderConsumer>();
+    cfg.AddRequestClient<SubmitOrder>();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Sample.Components/SubmitOrderConsumer.cs
@@ -0,0 +1,38 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Sample.Contracts;
+
+namespace Sample.Components;
+
+public class SubmitOrderConsumer : IConsumer<SubmitOrder>
+{
+    private readonly ILogger<SubmitOrderConsumer> _logger;
+
+    public SubmitOrderConsumer(ILogger<SubmitOrderConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task Consume(ConsumeContext<SubmitOrder> context)
+    {
+        _logger.Log(LogLevel.Debug, "SubmitOrderConsumer: {ConsumerNumber}", context.Message.CustomerNumber);
+
+        if (context.Message.CustomerNumber.Contains("TEST"))
+        {
+            await context.RespondAsync<OrderSubmissionRejected>(new
+            {
+                InVar.Timestamp,
+                context.Message.OrderId,
+                context.Message.CustomerNumber,
+                Reason = $"Test Customer cannot submit orders: {context.Message.CustomerNumber}"
+            });
+
+            return;
+        }
+
+        await context.RespondAsync<OrderSubmissionAccepted>(new { 
+            InVar.Timestamp, 
+            context.Message.OrderId, 
+            context.Message.CustomerNumber });
+    }
+}
